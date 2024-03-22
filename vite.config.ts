b85146e02049d12d388981e50e55837003290f4f
colder <cncolder@gmail.com>
2024-03-22 12:49:20 +0800

    fix: add slash before base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import { name } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig((env) => ({
-	base: env.command === "build" ? name : undefined,
+	base: env.command === "build" ? `/${name}` : undefined,
+
+	build: {
+		chunkSizeWarningLimit: 1024,
+	},
+
 	plugins: [react()],
 
 	test: {
